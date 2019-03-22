bc3ade3ad3d7ee29dbc32e06a40c4ea08a56a1c8
Anastasia <pototckaia@gmail.com>
2019-03-22 23:34:16 +1000

    add Event Parcelable

---
+++ app/src/main/java/com/example/calendar/data/Event.kt
@@ -0,0 +1,59 @@
+package com.example.calendar.data
+
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.serialization.Serializable
+import java.util.*
+
+class Event(var text:String, var begin: Long, var end: Long) : Parcelable {
+
+    constructor(text: String) : this(text, 0, 0) {
+        val startDate = Calendar.getInstance()
+        startDate.set(Calendar.HOUR, 1)
+        startDate.set(Calendar.MINUTE, 0)
+        startDate.set(Calendar.SECOND, 0)
+        val endDate = startDate.clone() as Calendar
+        endDate.set(Calendar.HOUR, 2)
+        begin = startDate.timeInMillis
+        end = endDate.timeInMillis
+    }
+
+    constructor() : this("", 0, 0)
+
+    var beginDate: Date
+        get() = Date(begin)
+        set(value) { begin = value.time }
+
+    var endDate: Date
+        get() = Date(end)
+        set(value) { end = value.time }
+
+
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readLong(),
+        parcel.readLong())
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest!!.let {
+            it.writeString(text)
+            it.writeLong(begin)
+            it.writeLong(end)
+        }
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Event> {
+        override fun createFromParcel(source: Parcel): Event {
+            return Event(source)
+        }
+
+        override fun newArray(size: Int): Array<Event> {
+            return Array<Event>(size, {Event()} )
+        }
+    }
+
+}
