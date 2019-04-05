e109a00643b3c4f99b83325ce2871ee63a3f01db
Nastya <pototckaia@gmail.com>
2019-04-05 20:21:26 +1000

    Add key to event id

--- app/src/main/java/com/example/calendar/helpers/Keys.kt
+++ app/src/main/java/com/example/calendar/helpers/Keys.kt
@@ -2,6 +2,7 @@ package com.example.calendar.helpers
 
 const val DATE_DAY_CALENDAR = "DATE_DATE_CALENDAR"
 
-const val EVENT_KEY = "EVENT_KEY"
+const val EVENT_ID_KEY = "EVENT_ID_KEY"
+
 const val START_EVENT_KEY = "DATE_BEGIN_KEY"
 const val END_EVENT_KEY = "DATE_END_KEY "
